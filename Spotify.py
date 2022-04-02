a3726ef7d1935f2c9b44bc50cb9e3c472f793fb0
Joshua Peachey <jp2571@bath.ac.uk>
2022-04-02 15:39:53 +0100

    put spotify templates in

--- Spotify.py
+++ Spotify.py
@@ -16,3 +16,27 @@ class Client:
 		# self.tracks = self.tracks[:-1]
 		# print(self.tracks)
 		return self.tracks
+
+#Get top 50 songs for a certain user
+def getTop50(userID):
+
+    ### Insert API jazz here ###
+    trackIDs = getTrackIDsOfTop50(userID)
+    tracks50 = getStatsOfAllTracks(trackIDs)
+
+    return tracks50 #This should return a python list containing all of the track objects
+
+def getTrackIDsOfTop50(userID):
+    
+    #Oh beautiful Matt do your thing
+
+    return TracksID
+
+def getStatsOfAllTracks(trackIds):
+
+    return TrackObjects #List of track objects
+
+
+# def getTrackAndStats(trackID):
+
+#     return TrackObject
