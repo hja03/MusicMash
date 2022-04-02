2cb637d5ffacc97617876a87b51479531e32a85b
Joshua Peachey <jp2571@bath.ac.uk>
2022-04-02 15:35:11 +0100

    templates!¬!!!

---
+++ spotityApiFunctions.py
@@ -0,0 +1,24 @@
+import Track
+
+#Get top 50 songs for a certain user
+def getTop50(userID):
+
+    ### Insert API jazz here ###
+
+    return tracks50 #This should return a python list containing all of the track objects
+
+def getTrackIDsOfTop50():
+    
+    #Oh beautiful Matt do your thing
+
+    return TracksID
+
+def getTrackAndStats(trackID):
+
+    return TrackObject
+
+def getStatsOfAllTracks(trackIds):
+
+    return TrackObjects #List of track objects
+
+
