f448c9af64fd6724e27f655b98f9019b6712fac5
Joshua Peachey <jp2571@bath.ac.uk>
2022-04-02 17:09:07 +0100

    most compatible tracks

--- generateAFusionPlaylist.py
+++ generateAFusionPlaylist.py
@@ -1,6 +1,11 @@
-def generate_a_fusion_playlist(user1Top50,user2Top50):
+def most_compatible_tracks(user1Top50,user2Top50):
     minCompat=90
+    tracksInPlaylist = []
 
-    for track in self.tracks:
-		track.compareSong()
+    for track in user1Top50:
+        compat = user2Top50.compare1TrackToAllSongs(track)
+        if compat > minCompat:
+            tracksInPlaylist.append(track)
+        
+    return tracksInPlaylist
     
