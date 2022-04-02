556502141f04f8ffea9d96d025e52a811cd988dc
Joshua Peachey <jp2571@bath.ac.uk>
2022-04-02 13:39:25 +0100

    layout for the top 50 function

---
+++ spotityApiFunctions.py
@@ -0,0 +1,8 @@
+import Track
+
+#Get top 50 songs for a certain user
+def getTop50(userID):
+
+    ### Insert API jazz here ###
+
+    return tracks50 #This should return a python list containing all of the track objects
