67193a36cee68d9406c31297ee0696367086f8c9
JoshuaPeachey <95437807+JoshuaPeachey@users.noreply.github.com>
2022-04-02 12:50:07 +0100

    Add files via upload
    This is the file to used to store tracks

---
+++ Track.py
@@ -0,0 +1,16 @@
+from tempfile import TemporaryDirectory
+
+
+class Track:
+    def __init__(self, danceability, energy, acousticness, valence, tempo):
+        self.danceability = danceability
+        self.energy = energy
+        self.acouticness = acousticness
+        self.valence = valence
+        self.tempo = tempo
+
+    def compareSong(otherPersonsSongs):
+        compatibility = 0 #0-100
+        return compatibility
+
+
